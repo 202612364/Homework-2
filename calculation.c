77cfea70afaeb32daeb7cdf4a9bb36efa306c4b6
202612364 <tasnimjhumka@gmail.com>
2026-09-24 13:28:02 +0900

    Add files via upload

---
+++ calculation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+    printf("Sum: %d\n", x + y);
+    printf("Difference: %d\n", x - y);
+    printf("Product: %d\n", x * y);
+    printf("Quotient: %d\n", x / y);
+    printf("Remainder: %d\n", x % y);
+    return 0;
+}
